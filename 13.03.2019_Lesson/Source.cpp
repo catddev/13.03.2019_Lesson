bb4a46d054875e95958890d96513c99af049f2aa
айкеновад <@204_13>
2019-03-14 11:46:09 +0600

    -

--- 13.03.2019_Lesson/Source.cpp
+++ 13.03.2019_Lesson/Source.cpp
@@ -104,26 +104,101 @@ int main()
 				in_file.getline(buffer, 50);
 				out_file << buffer << endl;
 			}
+			in_file.close();
+			out_file.close();
 		}
 		break;
-		case 6: 
+		case 6: // Дан текстовый файл.
+		//Необходимо создать новый файл, в который переписать из исходного файла все слова, состоящие не менее чем из семи букв.
 		{
+			ifstream in_file("out.txt");
+			ofstream out_file("in.txt");
+			char buffer[50];
+			int k = 0;
 
+			while (in_file >> buffer) //оператор ввода считывает до пробела, т.е. только слова
+			{
+				if (strlen(buffer) >= 7)
+					out_file << buffer << " " << endl;
+			}
+			in_file.close();
+			out_file.close();
 		}
 		break;
 		case 7:
+		//6.	Дан текстовый файл.
+		//Необходимо переписать его строки в другой файл.
+		//Порядок строк во втором файле должен быть обратным по отношению к порядку строк в заданном файле.
 		{
+
+			// как считать содержимое файла в массив строк
+
+			ifstream in_file("out.txt");
+			ofstream out_file("in.txt");
+			char str[20][50];
+			int k = 0;
+
+			while (in_file.getline(str[k], 50))
+				k++;
+			//for (int i = 0; i < k; i++)
+			//	cout << str[i] << endl;
+
+			for (int i = k; i >=0; i--)
+				out_file << str[i] << endl;
 
 		}
 		break;
-		case 8:
+		case 8:// Дан текстовый файл.Добавить в него строку из двенадцати черточек(------------),
+			// поместив ее после последней из строк, в которых нет пробела.
+			// Если таких строк нет, то новая строка должна быть добавлена после всех строк имеющегося файла.
+			//Результат записать в другой файл.
 		{
+			ofstream out_file("in.txt");
+			ifstream in_file("out.txt");
+
+			char buf[20][50];
+			int k = 0;
+			int pos = -1;
+
+			//strchr
+			while (in_file.getline(buf[k], 50))
+			{
+				if (!strchr(buf[k], ' ')) pos = k; //strchr определяет есть ли искомый символ в строке и возвращает указатель на него(адрес), а если нет - 0.
+				k++;
+			}
+			if (pos != -1)
+			{
+				for (int i = 0; i <= pos; i++)
+					out_file << buf[i] << endl;
+				out_file << "------------" << endl;
+				for (int i = pos + 1; i < k; i++)
+					out_file << buf[i] << endl;
+			}
+			else
+			{
+				for (int i = 0; i <= k; i++)
+					out_file << buf[i] << endl;
+				out_file << "------------" << endl;
+			}
 
 		}
 		break;
 		case 9:
+		// Дано два текстовых файла. Выяснить, совпадают ли их строки. Если нет, то вывести несовпадающую строку из каждого файла.
 		{
+			ifstream out_file("in.txt");
+			ifstream in_file("out.txt");
+			char buf[50];
+			char buf1[50];
 
+			while(!in_file.eof() || !out_file.eof()) //если && (и) то до конца самого короткого файла, а если || (или) то до конца длинного
+			{
+				in_file.getline(buf, 50);
+				out_file.getline(buf1, 50);
+
+				if (strcmp(buf, buf1) != 0)
+					cout << buf << "||" << buf1 << endl;
+			}
 		}
 		break;
 		case 10:
